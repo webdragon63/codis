47515ca9c89a24a356dd38bd543fb6bd5280c172
WebDragon <117004849+webdragon63@users.noreply.github.com>
2024-12-27 17:57:48 +0530

    Update codis.sh

--- codis.sh
+++ codis.sh
@@ -7,16 +7,19 @@ BLUE='\033[0;34m'
 CYAN='\033[0;36m'
 WHITE='\033[0;37m'
 
-echo -e "${VERMILION}                                 ┏━╸┏━┓╺┳┓╻┏━┓"
-echo -e "${WHITE}                                 ┃  ┃ ┃ ┃┃┃┗━┓"
-echo -e "${GREEN}                                 ┗━╸┗━┛╺┻┛╹┗━┛"
+echo -e "${VERMILION}                             ._____________________."
+echo -e "${VERMILION}                             |    ${WHITE}┏━╸┏━┓╺┳┓╻┏━┓    ${VERMILION}|"
+echo -e "${WHITE}                             |    ┃  ┃ ┃ ┃┃┃┗━┓    |"
+echo -e "${WHITE}                             |    ┗━╸┗━┛╺┻┛╹┗━┛    |"
+echo -e "${GREEN}                             |_____________________|"
+echo -e "${CYAN}                                         Made by: ${WHITE}Webdragon63"
+echo -e "${WHITE}________________________________________________________________________________"
 echo -e "${BLUE}  Ａ  ＭＵＬＴＩＴＨＲＥＡＤＥＤ  ＣＯＮＴＥＮＴ  ＤＩＳＣＯＶＥＲＹ  ＴＯＯＬ"
-echo -e "${BLUE}                        ＦＲＯＭ  ＷＥＢＤＲＡＧＯＮ６３"
-echo -e "${CYAN}--------------------------------------------------------------------------------"
-echo -e "${BLUE}"
+echo -e "${WHITE}^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo -e "${GREEN}"
 
 read -p "URL to discover >>" U
 read -p "Wordlist to use >>" W
-rm content_discovered.txt
-unbuffer python3 src/co-dis.py -u $U -w $W | tee /dev/tty | tee -a content_discovered.txt
+echo -e "${CYAN}"
+rm content_discovered.txt | unbuffer python3 src/co-dis.py -u $U -w $W | tee /dev/tty | tee -a content_discovered.txt
 echo -e "${WHITE}Process completed. Results saved as content_discovery.txt"
